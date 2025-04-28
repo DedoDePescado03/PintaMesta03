586013ddd80be934c2845dbdd6cf18d4ac6620ec
DedoDePescado03 <gaelloretomiranda@gmail.com>
2025-04-28 14:58:00 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PintaMesta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
