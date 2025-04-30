9277a6bdecfe0493500257b9ce10095f9c39bcb3
DedoDePescado03 <gaelloretomiranda@gmail.com>
2025-04-29 22:17:51 -0700

    Added connection to Supabase project

---
+++ InitializeSupabase.cs
@@ -0,0 +1,31 @@
+﻿using Supabase;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public static class SupabaseClientService
+{
+    public static Supabase.Client SupabaseClient { get; private set; }
+
+    public static async Task InitializeAsync()
+    {
+
+        try
+        {
+            var url = "https://sqohokjrvzkpyemexfuq.supabase.co";
+            var public_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxb2hva2pydnprcHllbWV4ZnVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MDUzOTcsImV4cCI6MjA2MDA4MTM5N30.pZZ0VuNOfRSP2ZDy9cW2IPdIrQCzSfKlaWV677cZ0a4";
+
+            SupabaseClient = new Supabase.Client(url, public_key, new SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            });
+
+            await SupabaseClient.InitializeAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+
+    }
+
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using Supabase.Interfaces;
+using Supabase;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace PintaMesta
 {
@@ -8,11 +11,27 @@ namespace PintaMesta
         public MainPage()
         {
             InitializeComponent();
+            InitializeSupabase();
         }
 
         private async void MoveToDrawPage(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync(nameof(DrawingPage));
+        }
+
+        private async void InitializeSupabase()
+        {
+            Debug.WriteLine("Connecting to Supabase...");
+
+            try
+            {
+                await SupabaseClientService.InitializeAsync();
+                Debug.WriteLine("Connected");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error trying to connect: {ex.Message}");
+            }
         }
 
     }
